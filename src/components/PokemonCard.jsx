b28dd9200f2ed7a613025d9de45ded2412728f6d
AXEL JANIAK <axeljpro@gmail.com>
2024-03-26 16:06:38 +0100

    basic 03

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,20 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
+const pokemon = pokemonList[0];
+
 function PokemonCard() {
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbizarre" />
-        <figcaption>Bulbizarre</figcaption>
+    return <figure className="card">
+        {pokemon.imgSrc ? <img className="card-img" src={pokemon.imgSrc} alt={pokemon.name}></img> : <p>???</p>}
+        <figcaption>{pokemon.name}</figcaption>
     </figure>
 }
 
